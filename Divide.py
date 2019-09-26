773280874a4dde786c49888cbff9f99e49cacf4d
Pavan  Patil <44057535+pavanpatil1@users.noreply.github.com>
2019-09-26 09:57:25 +0530

    Create Divide.py

---
+++ Divide.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the divisibleSumPairs function below.
+def divisibleSumPairs(n, k, ar):
+   return sum(1 for i in range(n) for j in range(n) if i < j and (ar[i]+ar[j])%k == 0)
+
+        
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
