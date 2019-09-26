fb5977ff2cc8345b4e60edde63ef874ff7ab04a6
Pavan  Patil <44057535+pavanpatil1@users.noreply.github.com>
2019-09-26 09:54:00 +0530

    Create Sock.py

---
+++ Sock.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    pairs = 0
+    for i in set(ar):
+        pairs += ar.count(i)//2
+
+    return pairs
+    # if(c%2==0):
+
+    #     return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
